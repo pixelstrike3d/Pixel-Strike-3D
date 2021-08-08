b7d2410462d7dcad5e95b9bd2a673bb479edcbdf
Pixel Strike 3D <84640174+pixelstrike3d@users.noreply.github.com>
2021-08-08 17:08:38 -0500

    Create database.h

---
+++ Variables/database.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <version.h>
+using namespace std;
+
+int main() {
+  string database = "http://pixelstrike3daws.com";
+  
+  if (database == "http://pixelstrike3daws.com")
+    cout << "Validated Database String" << endl;
+  else
+    cout << "Invalid Database String, Please Use The Default" << endl;
+  
+  
+    
+  return 0;
+}
