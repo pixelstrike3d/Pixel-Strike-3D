510654c56bd60095c4256650c6cfca5cb1055754
Pixel Strike 3D <84640174+pixelstrike3d@users.noreply.github.com>
2021-08-08 16:56:38 -0500

    Create vars.cpp

---
+++ Variables/vars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int version = 1.6;
+  string database = "http://pixelstrike3daws.com";
+  
+  if version == 1.6
+    cout << "Version 1.6 Validated Successfully" << endl;
+  else
+    cout << "Please update to a newer version" << endl;
+    
+  return 0;
+}
