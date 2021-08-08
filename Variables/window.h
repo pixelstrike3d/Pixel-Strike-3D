fd29d45024c62d87050ebf6adabfeeb870bdb48b
Pixel Strike 3D <84640174+pixelstrike3d@users.noreply.github.com>
2021-08-08 17:10:29 -0500

    Create window.h

---
+++ Variables/window.h
@@ -0,0 +1,12 @@
+// Resource identifiers, base and subordinate windows
+#define IDR_MAINFRAME		128
+
+#define IDD_ABOUTDLG		130
+
+// ...menu items
+#define IDM_FILE			200
+#define ID_FILE_OPEN		(IDM_FILE + 1)
+#define ID_APP_EXIT			(IDM_FILE + 99)
+
+#define IDM_HELP			300
+#define ID_APP_ABOUT		(IDM_HELP + 1)
