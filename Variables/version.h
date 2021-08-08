6a0283e0f39b73f19b75b2b42901260df1a18fe2
Pixel Strike 3D <84640174+pixelstrike3d@users.noreply.github.com>
2021-08-08 17:06:29 -0500

    Update version.h

--- Variables/version.h
+++ Variables/version.h
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
   int majorVersion = 1;
   int minorVersion = 6;
-  string database = "http://pixelstrike3daws.com";
   
   if (majorVersion == 1 and minorVersion == 6)
     cout << "Version 1.6 Validated Successfully" << endl;
