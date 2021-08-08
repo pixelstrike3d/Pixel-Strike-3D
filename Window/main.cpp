69a1c9405ea1612e2767dec8f783e14736cdc673
Pixel Strike 3D <84640174+pixelstrike3d@users.noreply.github.com>
2021-08-08 17:39:29 -0500

    Update main.cpp

--- Window/main.cpp
+++ Window/main.cpp
@@ -7,6 +7,21 @@
 using namespace std;
 
 int main() {
+  string database = "http://pixelstrike3daws.com";
+  
+  if (database == "http://pixelstrike3daws.com")
+    cout << "Validated Database String" << endl;
+  else
+    cout << "Invalid Database String, Please Use The Default" << endl;
+  
+    int majorVersion = 1;
+  int minorVersion = 6;
+  
+  if (majorVersion == 1 and minorVersion == 6)
+    cout << "Version 1.6 Validated Successfully" << endl;
+  else
+    cout << "Please update to a newer version" << endl;
+  
   int loaded = 1;
   
   if (loaded == 1)
